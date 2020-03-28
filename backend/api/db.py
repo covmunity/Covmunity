ee85d768e5a88ebaf91444419cf8ee8ee8ce5aa7
Jaime Soriano Pastor <jsorianopastor@gmail.com>
2020-03-28 20:30:33 +0100

    Add missing files

---
+++ backend/api/db.py
@@ -0,0 +1,31 @@
+import click
+import time
+
+from flask.cli import AppGroup
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+db_cli = AppGroup('db')
+
+@db_cli.command('init')
+def init_db():
+    """
+    Create schema
+    """
+    db.create_all()
+
+@db_cli.command('wait')
+def wait_db():
+    """
+    Wait database to be available during 30 seconds
+    """
+    retries = 30
+    for i in range(retries):
+        try:
+            db.session.execute('SELECT 1')
+            return
+        except Exception as e:
+            if i >= retries-1:
+                raise e
+            time.sleep(1)
