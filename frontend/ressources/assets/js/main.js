e0364ea82cabdf4942a1a0a33ec5a7b5ad5beed3
Jiab77 <jonathan.barda@gmail.com>
2020-03-29 04:12:50 +0200

    Several changes:  - Moved UI code to separated file  - Fixed basic routing code  - Added some test routes  - Added missing comments  - Tried to clean up a bit the whole

--- frontend/ressources/assets/js/main.js
+++ frontend/ressources/assets/js/main.js
@@ -1,260 +1,46 @@
 "use strict";
 
-// internal state
-window.init = {
-	theme: ''
-};
-
 // internal object
 var covmunity = covmunity || {};
 
-// app ui
-$(document).ready(function() {
-	// Disabled links
-	$('a[href="#!"]').on('click', function(event) {
-		event.preventDefault();
-	});
-
-	// Fix top menu when passed
-	$('.ui.large.secondary.inverted.menu').visibility({
-		once: false,
-		onBottomPassed: function() {
-			$('.fixed.menu').transition('fade in');
-		},
-		onBottomPassedReverse: function() {
-			$('.fixed.menu').transition('fade out');
-		}
-	});
-
-	// Create sidebar and attach to menu open
-	$('.ui.sidebar')
-		// .sidebar('setting', { transition: 'scale down', mobileTransition: 'scale down' })
-		.sidebar('setting', { transition: 'overlay', mobileTransition: 'overlay' })
-		.sidebar('attach events', '.toc.item');
-	
-	// Dropdowns
-	$('.ui.dropdown').dropdown({
-		on: 'hover'
-	});
-
-	// Accordions
-	$('.ui.accordion').accordion();
-
-	// Dismissable messages
-	$('.message .close').on('click', function() {
-		$(this).closest('.message').transition('fade');
-	});
-
-	// Tooltips
-	$('.tooltipped').popup();
-
-	// Scrolling tables
-	// TODO: Add throttling...
-	$('.scrolling-table').on('scroll', function (event) {
-		// console.log('User is scrolling the table content.', event);
-		// console.info('Scroll position:', event.target.scrollTop);
-		// console.info('This:', $(this));
-		// console.info('Table header:', $(this).find('.ui.table.sticky-headed thead tr:first-child > th'));
-
-		// Store scroll position
-		var pos = event.target.scrollTop;
-
-		// Target next table with sticky headers
-		var $tableHeaders = $(this).find('.ui.table.sticky-headed thead tr:first-child > th');
-
-		// Set a darker background color when user is scrolling table content
-		if (pos !== 0) {
-			if (!$tableHeaders.hasClass('darken')) {
-				$tableHeaders.addClass('darken');
-			}
-		}
-		else {
-			if ($tableHeaders.hasClass('darken')) {
-				$tableHeaders.removeClass('darken');
-			}
-		}
-	});
-
-	// ui theme elements
-	var $themeElements = [
-		{ name: 'dividingHeaders', target: $('.ui.dividing.header') },
-		{ name: 'iconHeaders', target: $('.ui.icon.header') },
-		{ name: 'tooltippedIcons', target: $('.tooltipped.icon') },
-		{ name: 'cardsContainer', target: $('.ui.cards') },
-		{ name: 'cards', target: $('.ui.card') },
-		{ name: 'dropdowns', target: $('.ui.dropdown') },
-		{ name: 'fixedMenu', target: $('.ui.top.fixed.menu') },
-		{ name: 'breadcrumb', target: $('.ui.breadcrumb') },
-		{ name: 'tables', target: $('.ui.table') },
-		{ name: 'segments', target: $('.ui.segment').not('.inverted') },
-		{ name: 'placeholders', target: $('.ui.placeholder') }
-	];
-	var $themeValue = $('#theme-value');
-	var $darkThemeButton = $('div.right.menu div#dark-theme');
-	var $lightThemeButton = $('div.right.menu div#light-theme');
-
-	// query light / dark theme selection
-	if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
-		console.log('🎉 Dark mode is supported');
-
-		// detect dark mode
-		var darkMatcher = window.matchMedia('(prefers-color-scheme: dark)');
-
-		// attach dark mode listener
-		darkMatcher.addListener(onUpdate);
-
-		// remove listener on window unload
-		window.unload = function (event) {
-			console.log('window unloaded.', event);
-			console.log('removing listeners.');
-
-			darkMatcher.removeListener(onUpdate);
-		}
-
-		// set and display initial theme value
-		$themeValue.text((darkMatcher.matches === true ? 'dark' : 'light') + ' (media-query)');
-		window.init.theme = (darkMatcher.matches === true ? 'dark' : 'light');
-
-		// initial theme buttons state
-		if (darkMatcher.matches === true) {
-			$lightThemeButton.toggleClass('hide');
-		}
-		else {
-			$darkThemeButton.toggleClass('hide');
-		}
-
-		// apply initial theme
-		applyTheme();
-	}
-
-	// light / dark theme buttons
-	$darkThemeButton.on('click', function (event) {
-		console.log('Theme selector clicked.', event);
-
-		// inverted logic because of animated buttons
-		window.init.theme = 'light';
-
-		// display active theme
-		$themeValue.text(window.init.theme + ' (user-event)');
-
-		// delayed button change
-		var toggleState = setTimeout(function(){
-			switchButtons();
-			clearTimeout(toggleState);
-		}, 600);
-
-		// apply defined theme
-		applyTheme();
-	});
-	$lightThemeButton.on('click', function (event) {
-		console.log('Theme selector clicked.', event);
-
-		// inverted logic because of animated buttons
-		window.init.theme = 'dark';
-
-		// display active theme
-		$themeValue.text(window.init.theme + ' (user-event)');
-
-		// delayed button change
-		var toggleState = setTimeout(function(){
-			switchButtons();
-			clearTimeout(toggleState);
-		}, 600);
-
-		// apply defined theme
-		applyTheme();
-	});
-
-	// light / dark theme event handler
-	function onUpdate(event) {
-		console.log('Theme changed.', event);
-		console.log('Previous theme value:', window.init.theme);
-
-		// set and display gathered theme value
-		window.init.theme = (event.matches === true ? 'dark' : 'light');
-		$themeValue.text(window.init.theme + ' (media-event)');
-
-		// toggle theme buttons
-		switchButtons();
-
-		// apply gathered theme
-		applyTheme();
-	}
-
-	// light / dark theme button toggler
-	function switchButtons() {
-		$darkThemeButton.toggleClass('hide');
-		$lightThemeButton.toggleClass('hide');
-	}
-
-	// light / dark theme apply
-	function applyTheme() {
-		console.log('Theme applied.', (!event ? '(auto)' : event));
-		console.log('New theme value:', window.init.theme);
-		console.log('Dark mode is ' + (window.init.theme === 'dark' ? '🌒 on' : '☀️ off') + '.');
-		console.log('Theme elements:');
-		console.table($themeElements);
-		$($themeElements).each(function () {
-			console.log('Styling element [' + $(this)[0].name + ']:', $(this)[0].target);
-		});
-
-		switch (window.init.theme) {
-			case 'dark':
-				$($themeElements).each(function () {
-					var $target = $(this)[0].target;
-
-					// Apply dark theme
-					$target.addClass('inverted');
-
-					// Remove uggly extra shadow
-					if ($target.hasClass('floating')) {
-						$target.removeClass('floating');
-						$target.addClass('floating-disabled');
-					}
-				});
-
-				// Apply dark theme on tooltips
-				$('.tooltipped').attr('data-variation', 'inverted');
-				break;
-		
-			case 'light':
-			default:
-				$($themeElements).each(function () {
-					var $target = $(this)[0].target;
-					
-					// Remove dark theme
-					$target.removeClass('inverted');
-
-					// Add nice floating shadow
-					if ($target.hasClass('floating-disabled')) {
-						$target.removeClass('floating-disabled');
-						$target.addClass('floating');
-					}
-				});
-
-				// Remove dark theme on tooltips
-				$('.tooltipped').attr('data-variation', '');
-				break;
-		}
-	}
-});
-
 // dirty config structure
 covmunity.config = {
-	debug: true
+	debug: true // can be used to enable / disable console output
 };
 
 // dirty frontend -> backend api code
 covmunity.api = {
+	//
+	// The API hooks from Fomantic-UI can also be used.
+	// See them before coding? :D
+	// - https://fomantic-ui.com/behaviors/api.html
+	//
+	// It won't conflict with 'page.js' as 'page.js' is reacting only
+	// when a 'click' event is matching a defined route.
+	//
+	// In case of issues on 'page.js' side, the last '*' route might be called.
+	//
+
 	getAccount: function (ctx, next) {
 		console.group('API');
 		console.log('[account]', ctx);
 		console.groupEnd();
+		
 		// api call account details
 		var account = getAccount();
-		//TODO: prepare/forward JSobject to UI element
+
+		// TODO: prepare/forward JSobject to UI element*
+		// 
+		// * The 'ctx' object can be used to store data that is transient between pages
+		//
+		// See: 'Context' method in the 'page.js' documentation
+		// - https://github.com/visionmedia/page.js#context
+
+		// debug output
 		console.log(account.email)
-		next(); // callback is here just for an indication of the behaviour
+
+		next(); // callback is here just for an indication of the behaviour (exactly)
+		// If removed, the second passed callback defined in the route won't be called.
 	},
 };
 
@@ -266,26 +52,28 @@ covmunity.app = {
 		console.groupEnd();
 	},
 	run: function () {
+		// Call the above init function.
+		// Can be useful for loaders or things similar.
 		this.init();
 
-		/*
-		TODO: Fix this part. I'm gonna eat something
-		*/
+		//
+		// Call the URL handler 'page.js'
+		//
+		// When 'click' is set to true, all navigation clicks will be captured
+		// then handled by the specified method / class defined for the matching route.
+		//
+		// Routes are defined at the bottom of this file.
+		//
+		// See documentation here:
+		// - https://github.com/visionmedia/page.js
+		//
 
-		// page();
+		// Here we are setting the run options of 'page.js'
 		page({
-			click: true,
-			dispatch: true,
-			popstate: true
+			click: true, // Listen on 'click' events or not
+			dispatch: false, // Dispatch request to backend or not (beware that the request will still get dispatched on page reloads)
+			popstate: true // Change address bar URL or not (also populate the history stack)
 		});
-		/* page({
-			click: false, // Change this to get back on client-side only
-			popstate: false, // Change this to get back on client-side only
-		}); */
-		/* page({
-			click: true, // Change this to get back on client-side only
-			popstate: true, // Change this to get back on client-side only
-		}); */
 	},
 };
 
@@ -303,33 +91,59 @@ covmunity.pages = {
 
 		next(); // Must be the last line, uncomment to cascade callbacks
 	},
+	account: function (ctx) {
+		console.group('Page.js');
+		console.info('Loading [account] client code...', ctx);
+		console.groupEnd();
+
+		// do the display logic here
+	},
 	hello: function (ctx) {
 		console.group('Page.js');
-		console.info('Client [hello] API call visual return', ctx);
+		console.info('Loading [hello] client code...', ctx);
+		console.groupEnd();
+
+		// do the display logic here
+	},
+	help: function (ctx) {
+		console.group('Page.js');
+		console.info('Loading [help] client code...', ctx);
 		console.groupEnd();
 
 		// do the display logic here
 	},
 	home: function (ctx) {
 		console.group('Page.js');
-		console.info('Welcome to the Homepage !', ctx);
+		console.info('Loading [home] client code...', ctx);
 		console.groupEnd();
 
-		// next(); // Must be the last line, uncomment to cascade callbacks
+		// do the display logic here
 	},
 };
 
 // Define all client-side routes - Must be same as server ones
 // Just replace '@' by ':' for parameters
-page('*', covmunity.api.getAccount, covmunity.pages.init);
-page('/', covmunity.pages.home);
-page('/account/profile', covmunity.api.getAccount, covmunity.pages.hello);
-page('*', covmunity.pages.error);
+// page('*', covmunity.api.getAccount, covmunity.pages.init); // It should work with just the 'init' method
+page('*', covmunity.pages.init); // Initialize on every requests (no need to change here normally)
+page('/', covmunity.pages.home); // Call the mode that will render the 'home' content
+page('/help', covmunity.pages.help); // Call the mode that will render the 'home' content
+page('/account', covmunity.pages.account); // Call the mode that will render the 'account' content
+page(
+	'/profile', // Client side URL
+	covmunity.api.getAccount, // API method to send request to backend
+	covmunity.pages.account // When request is sent to backend, call the associated render method
+);
+page('*', covmunity.pages.error); // Will be called on any errors, like 404, 500, etc...
 
 // Boot stuff when DOM is loaded
-$(function (event) {
+$(function () {
 	console.group('App');
-	console.info('DOM Loaded.', event);
+	console.info('DOM Loaded.');
+
+	// Once the DOM is loaded, we are calling our 'page.js' wrapper.
+	// This wrapper will call 'page.js' which then, will parse the defined routes above.
+	// This is how the main app code is started.
 	covmunity.app.run();
+
 	console.groupEnd();
 });
